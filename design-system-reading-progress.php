564d9d9616c2897f4f07805fd5c2fef9dac2c176
Ruben <contact@rubenmadila.com>
2024-03-30 10:35:42 +0000

    Up version

--- design-system-reading-progress.php
+++ design-system-reading-progress.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Design System Scrolling Progress
  * Description:       An interactive block with the Interactivity API
- * Version:           0.1.0
+ * Version:           0.1.1
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Author:            The WordPress Contributors
